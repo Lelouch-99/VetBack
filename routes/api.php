3c43775f4bbd944a7f4faaecedd75982cc451bfe
Luis Francisco Benítez Núñez <luisbeniteznunez@gmail.com>
2023-04-10 21:44:45 -0400

    se modifica la ruta

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/contact', [VetBackController::class, 'store']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('contact', [VetBackController::class, 'store']);
 
